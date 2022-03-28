17057e8adaddef102f931dfddac01e2dff89c06e
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2022-03-28 22:30:10 +0500

    done

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const PORT = 8080
+
+app.use(express.json())
+app.use(cors())
+
+// app.get('/', (req, res) => {
+//     try {
+//         console.log(arr);
+//         return res.send({ message: 'Hello NodeJs', success: true })
+//     } catch (e) {
+//         console.log('e', e);
+//         return res.send({ message: e?.message, success: false })
+//     }
+// })
+
+// app.post('/login', (req, res) => {
+//     try {
+//         console.log('req', req?.body);
+//         return res.send({ message: 'Successfully Loggedin!', success: true })
+//     } catch (e) {
+//         console.log('e', e);
+//         return res.send({ message: e?.message, success: false })
+//     }
+// })
+
+app.use('/', require('./routes'))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}!! `);
+})
